9a6f01bf1a82b1ffe34f89d60cab7a9a217d9c5f
subashc069 <bcoolboy8@gmail.com>
2019-09-03 17:14:29 +0545

    first elequent relationship

--- app/Answer.php
+++ app/Answer.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Question;
 
 class Answer extends Model
 {
-    //
+    protected $guarded =[];
+
+    public function question()
+    {
+        $this->belongsTo(Question::class);
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use app\Answer;
 
 class Question extends Model
 {
     protected $guarded =[];
+    
+    public function answer()
+    {   
+        $this->hasMany(Answer::class);
+    }
 }
